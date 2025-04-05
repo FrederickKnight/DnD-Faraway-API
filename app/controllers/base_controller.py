fcb12ea794b1399805570140efad03faeb0af576
Frederick Knight <infisefir@gmail.com>
2025-04-05 20:42:03 +0000

    Refactor[Routes]: Refactor the routes to work in a base route for easy manipulation in the future

--- app/controllers/base_controller.py
+++ app/controllers/base_controller.py
@@ -1,6 +1,6 @@
 from app import db
 from app.error_handler import error_handler
-from flask import Response,json
+from flask import Response,json,Request
 
 
 class BaseController:
@@ -11,18 +11,24 @@ class BaseController:
         self.session = db.session  
         
         
-    def controller_get_all(self):
+    def controller_get_all(self,request:Request):
+        
+        version = request.headers.get("Accept")
+        
         _query = self.session.query(self.__model).all()
-        return self.__return_json__(_query)
+        return self.__return_json__(_query,version)
         
     
-    def controller_register(self,data):
+    def controller_register(self,request:Request):
         
-        if "id" in data:
-            data["id"] = None
+        json_request = request.get_json()
+        version = request.headers.get("Accept")
+        
+        if "id" in json_request:
+            json_request["id"] = None
             
         try:
-            new_data = self.__model(**{**self.__defaults,**data})
+            new_data = self.__model(**{**self.__defaults,**json_request})
             self.session.add(new_data)
             self.session.commit()
             
@@ -30,28 +36,32 @@ class BaseController:
             self.session.rollback()
             return error_handler(e,"Error in commit")
         
-        return self.__return_json__(self.session.query(self.__model).filter_by(id = new_data.id).all())
+        return self.__return_json__(self.session.query(self.__model).filter_by(id = new_data.id).all(),version)
         
         
-    def controller_update(self,id = None,data = None):
+    def controller_update(self,id = None,request:Request = None):
+        
+        json_request = request.get_json()
+        version = request.headers.get("Accept")
+        
         if not id or not isinstance(id,int):
-            if "id" in data:
-                _id = data["id"]
+            if "id" in json_request:
+                _id = json_request["id"]
             else:
-                return Response(response=json.dumps({}),status=400,mimetype="application/json")
+                return Response(response=json.dumps({"message":"Not id in data"}),status=400,mimetype="application/json")
         else:
             _id = id
-            if "id" in data:
-                return Response(response=json.dumps({}),status=400,mimetype="application/json")
+            if "id" in json_request:
+                return Response(response=json.dumps({"message":"id in data and url"}),status=400,mimetype="application/json")
         
         self.__defaults["id"] = _id
         
         _query = self.__query_id__(_id)
         
         if not _query:
-            return self.__return_json__(_query)
+            return self.__return_json__(_query,version)
         
-        new_data = {**self.__defaults,**data} 
+        new_data = {**self.__defaults,**json_request} 
         
         try:
             for key,value in new_data.items():
@@ -66,9 +76,9 @@ class BaseController:
             self.session.rollback()
             return error_handler(e,"Error in commit")
         
-        return self.__return_json__(_query)
+        return self.__return_json__(_query,version)
     
-    def controller_delete(self,id):    
+    def controller_delete(self,id):
         _query = self.__query_id__(id)
         
         try:
@@ -82,28 +92,50 @@ class BaseController:
         return Response(status=204)
 
 
-    def controller_get_by_id(self,id):
-        return self.__return_json__(self.__query_id__(id))
+    def controller_get_by_id(self,id,request:Request):
+        version = request.headers.get("Accept")
+        return self.__return_json__(self.__query_id__(id),version)
 
 
     ### Helpers
-    def __return_json__(self,items_query):
-        _response = []
-        try:
-            if(isinstance(items_query,list) or isinstance(items_query,dict)):
-                _response = [item.get_dict() for item in items_query]
-            else:
-                _response = [items_query.get_dict()]
-        except:
-            return items_query
+    def __return_json__(self,items,version:str = None):
+        _version = version if version != None else "v1"
+    
+        if isinstance(items,Response):
+            return items
         
-        return {
-            "data":_response,
-            "metadata":{
-                "type":str(self.__model().__getClassName__()),
-                "size":len(_response)
+        if isinstance(items,dict) or isinstance(items,list):
+            _response = [item.get_dict() for item in items]
+
+        else:
+            _response = [items.get_dict()]
+        
+        if "dndfaraway.v1" in _version:
+            return {
+                "data":_response,
+                "metadata":{
+                    "type":str(self.__model().__getClassName__()),
+                    "size":len(_response)
+                }
             }
-        }
+            
+        if "dndfaraway.v2" in _version:
+            return {
+                "response":_response,
+                "metadata":{
+                    "type":str(self.__model().__getClassName__()),
+                    "size":len(_response),
+                    "api_version":"v2"
+                }
+            }
+            
+        else:
+            error = {
+                "error": "Incorrect Version of API",
+                "message": "The given Version of the API is incorrect or null",
+                "details" : "Expected dndfaraway.[version]"
+            }
+            return error_handler(error,"Error in Versioning")
         
         
     def __query_id__(self,_id):
@@ -117,5 +149,5 @@ class BaseController:
         else:
             _query = self.session.query(self.__model).filter_by(id = _id).first()
             if not _query:
-                return Response(response=json.dumps({}),status=404,mimetype="application/json")
+                return Response(status=404)
             return _query
--- app/routes/spells/spell_route.py
+++ app/routes/spells/spell_route.py
@@ -2,32 +2,8 @@ from flask import Blueprint,request
 
 from app.controllers.spells.spell_controller import SpellController
 
+from app.routes.base_route import BaseRoute
+
 spell_bp = Blueprint("spell",__name__)
 
-
-@spell_bp.route("/",methods=["GET"])
-def get_all_spell():
-    return SpellController().controller_get_all()
-
-@spell_bp.route("/add",methods=["POST"])
-def add_spell():
-    json_request = request.get_json()
-    return SpellController().controller_register(json_request)
-
-@spell_bp.route("/update",methods=["PUT"])
-def update_spell():
-    json_request = request.get_json()
-    return SpellController().controller_update(data=json_request)
-
-@spell_bp.route("/<int:id>/update",methods=["PUT"])
-def update_by_id_spell(id):
-    json_request = request.get_json()
-    return SpellController().controller_update(id=id,data=json_request)
-
-@spell_bp.route("/<int:id>/delete",methods=["DELETE"])
-def delete_spell(id):
-    return SpellController().controller_delete(id)
-
-@spell_bp.route("/<int:id>",methods=["GET"])
-def get_by_id_spell(id):
-    return SpellController().controller_get_by_id(id)
+spell_route = BaseRoute(spell_bp,SpellController())
